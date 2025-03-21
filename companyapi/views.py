8941db40951f07c0455d4708d1aa1f4619cf26f9
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-03-21 20:56:47 +0500

    first

--- companyapi/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-
-
-def home_page(request):
-    print('home_page')
-    friends = ['John', 'Jane', 'Jack']
-    return JsonResponse(friends, safe=False)  
